b400730dccbaec936318277d2102083850c12896
Emmanuelle Esguerra <emman.esguerra2013@gmail.com>
2025-06-15 09:47:34 +0800

    make the table responsive

--- src/pages/VocabPage.vue
+++ src/pages/VocabPage.vue
@@ -1,35 +1,32 @@
 <template>
-  <div class="container py-4">
-    <div class="mb-4">
-      <h3 class="d-inline">Vocabulary List -</h3>
-      <select
-        id="lesson-select"
-        v-model="selectedLesson"
-        class="form-select d-inline w-auto ms-2"
-      >
-        <option v-for="n in 50" :key="n" :value="n">Lesson {{ n }}</option>
-      </select>
-    </div>
+    <div class="container py-4">
+        <div class="mb-4">
+            <h3 class="d-inline">Vocabulary List -</h3>
+            <select id="lesson-select" v-model="selectedLesson" class="form-select d-inline w-auto ms-2">
+                <option v-for="n in 50" :key="n" :value="n">Lesson {{ n }}</option>
+            </select>
+        </div>
 
-    <div class="table-responsive">
-      <table class="table table-striped table-bordered">
-        <thead class="table-dark">
-          <tr>
-            <th>Kanji</th>
-            <th>Kana</th>
-            <th>Meaning</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item, index) in filteredVocabularies" :key="index">
-            <td>{{ item.kanji }}</td>
-            <td>{{ item.kana }}</td>
-            <td class="text-start">{{ item.meaning }}</td>
-          </tr>
-        </tbody>
-      </table>
+        <div class="table-responsive" style="max-height: 70vh; overflow-y: auto;">
+            <table class="table table-striped table-bordered mb-0">
+                <thead class="table-dark sticky-top">
+                    <tr>
+                        <th>Kanji</th>
+                        <th>Kana</th>
+                        <th>Meaning</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, index) in filteredVocabularies" :key="index">
+                        <td>{{ item.kanji }}</td>
+                        <td>{{ item.kana }}</td>
+                        <td class="text-start">{{ item.meaning }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
     </div>
-  </div>
 </template>
 
 <script setup>
@@ -39,6 +36,6 @@ import { vocabularies } from '@/data/vocabularies.js'
 const selectedLesson = ref(1)
 
 const filteredVocabularies = computed(() =>
-  vocabularies.filter(v => v.lesson === selectedLesson.value)
+    vocabularies.filter(v => v.lesson === selectedLesson.value)
 )
 </script>
