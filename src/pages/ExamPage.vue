542aa067e7d7db4e02d3a81f1779fe45dcbd701d
Emmanuelle Esguerra <emman.esguerra2013@gmail.com>
2025-08-20 14:48:38 +0800

    warn user on leaving or refreshing exam page

--- src/pages/ExamPage.vue
+++ src/pages/ExamPage.vue
@@ -3,7 +3,7 @@
         <!-- Exam in progress -->
         <div v-if="!exam.finished && exam.currentQuestion" class="question-box">
 
-            <component :is="exam.currentQuestion.entry.pos === 'kanji' ? KanjiQuestion : RegularQuestion"
+            <component :is="exam.currentQuestion.entry.type === 'kanji' ? KanjiQuestion : RegularQuestion"
                 :question="exam.currentQuestion" :selected-answer="selectedAnswer" @select-answer="selectAnswer"
                 @submit="handleNext" />
 
@@ -17,12 +17,13 @@
 </template>
 
 <script setup>
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, onBeforeUnmount } from 'vue'
 import { useQuizStore } from '@/stores/quizStore'
 import { useExamStore } from '@/stores/examStore'
 import Result from "@/components/Result.vue"
 import RegularQuestion from "@/components/RegularQuestion.vue"
 import KanjiQuestion from "@/components/KanjiQuestion.vue"
+import { onBeforeRouteLeave } from 'vue-router'
 
 const quizStore = useQuizStore()
 const exam = useExamStore()
@@ -30,6 +31,37 @@ const exam = useExamStore()
 onMounted(() => {
     if (!exam.started) {
         exam.init(quizStore.settings)
+    }
+
+    // Warn on refresh / close
+    window.addEventListener('beforeunload', handleBeforeUnload)
+})
+
+onBeforeUnmount(() => {
+    window.removeEventListener('beforeunload', handleBeforeUnload)
+})
+
+// Warn on browser refresh or tab close
+function handleBeforeUnload(event) {
+    if (!exam.finished) {
+        event.preventDefault()
+        event.returnValue = ''
+    }
+}
+
+// Warn on route change only for this page
+onBeforeRouteLeave((to, from, next) => {
+    if (!exam.finished) {
+        const answer = confirm(
+            "Leaving this page will reset the exam. Are you sure?"
+        )
+        if (answer) {
+            next()
+        } else {
+            next(false)
+        }
+    } else {
+        next()
     }
 })
 
